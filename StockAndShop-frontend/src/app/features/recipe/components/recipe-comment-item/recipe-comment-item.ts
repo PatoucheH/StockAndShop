5eea8bc4fbcf1a84a14792418db2c50204b76c3e
PatoucheH <hugopat@live.be>
2026-06-30 14:23:45 +0200

    Add : btn delete only viewable by comment's user

--- StockAndShop-frontend/src/app/features/recipe/components/recipe-comment-item/recipe-comment-item.ts
+++ StockAndShop-frontend/src/app/features/recipe/components/recipe-comment-item/recipe-comment-item.ts
@@ -4,6 +4,7 @@ import { RecipeComment } from '../../../../shared/models/recipe.models';
 import { StarRatingComponent } from '../../../../shared/components/star-rating/star-rating';
 import { RecipeCommentService } from '../../services/recipe-comment.service';
 import { ToastService } from '../../../../core/services/toast.service';
+import { AuthService } from '../../../auth/services/auth.service';
 
 @Component({
   selector: 'app-recipe-comment-item',
@@ -14,12 +15,15 @@ export class RecipeCommentItemComponent {
 
   recipeCommentService = inject(RecipeCommentService);
   toastService = inject(ToastService);
+  authService = inject(AuthService);
   comment = input.required<RecipeComment>();
 
   displayName = computed(() => {
     const name = this.comment().username;
     return name.includes('@') ? name.split('@')[0] : name;
   });
+
+  isOwner = computed(() => this.comment().username === this.authService.getUserEmail());
 
   deleteComment(id: number) {
     this.recipeCommentService.deleteComment(id).subscribe({
